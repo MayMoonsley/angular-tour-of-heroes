79d1fdd1576bf5b6491d03a41835877d4c9b7684
May Lawver <may@maycod.es>
2023-10-02 17:34:04 -0400

    Add hero name two-way binding

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+    hero: Hero = {
+        name: 'Windstorm',
+        id: 1
+    };
+}
