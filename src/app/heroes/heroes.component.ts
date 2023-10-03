f2c35f8b071cc1b1f4107ba7476e88df6d180a3b
May Lawver <may@maycod.es>
2023-10-03 10:34:47 -0400

    Begin on app routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-    selectedHero?: Hero;
     heroes: Hero[] = [];
 
     constructor(private heroService: HeroService, private messageService: MessageService) { }
@@ -20,10 +19,5 @@ export class HeroesComponent {
 
     getHeroes(): void {
         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-        this.messageService.add(`HeroesService: Selected hero id=${hero.id}`)
     }
 }
