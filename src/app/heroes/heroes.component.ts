0345d838f902d04a3f30940c1b4a14d68c7312d6
May Lawver <may@maycod.es>
2023-10-03 09:26:20 -0400

    Add HeroService & dependency injection

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock_heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +9,17 @@ import { HEROES } from '../mock_heroes';
 })
 export class HeroesComponent {
     selectedHero?: Hero;
-    heroes: Hero[] = HEROES;
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
